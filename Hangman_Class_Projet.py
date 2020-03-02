79cb3beac6ff1f84a2bbd179b3b42cac855a2cfa
aepell <60886131+aepell@users.noreply.github.com>
2020-03-02 10:26:26 -0500

    Add files via upload

--- Hangman_Class_Projet.py
+++ Hangman_Class_Projet.py
@@ -1,5 +1,34 @@
+import random
+WORDLIST = [""]
+updatedWord = ""
 secret = ""
 letter = ""
+
+def initialize():
+    global updatedWord
+    global SECRET
+    WORDLIST = ['one','two','three','four','five','six','seven','eight','nine','ten']
+    SECRET = random.choice(WORDLIST)
+    updatedWord = [('_'*len(SECRET))]
+   
+    print(updatedWord)
+    print SECRET
+
+def getLetter():
+    global letter
+    letter = str(raw_input('ENTER YOUR GUESS: '))
+    print ('YOUR GUESS: ' + letter)
+
+def fillLetter():
+    pos = SECRET.find(letter)
+    updatedWord[pos] = letter
+    
+def ifWon():
+    if updatedWord == SECRET:
+        print ('You Won!')
+    else: 
+        getLetter()
+        
 def test():
     if (letter in secret):
         pos = secret.find(letter)
@@ -8,3 +37,9 @@ def test():
         ifWon()
     else:
         getLetter()
+        
+def main():
+    initialize()
+    getLetter()
+    
+
